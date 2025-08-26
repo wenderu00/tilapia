20cfc2c16b43493679ea29f12c9b8caa7f13ca44
Márcio Wendell <mwmcj@cin.ufpe.br>
2025-08-25 21:44:32 -0300

    front in progress

---
+++ tilapia/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <header>
+
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        
+      </footer>
+    </div>
+  );
+}
